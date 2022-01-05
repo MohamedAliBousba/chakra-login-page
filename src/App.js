da04d296deff2a8dbfbaf5e386a78b4beae59193
Mohamed Ali <im_bousba@esi.dz>
2022-01-05 16:25:08 +0100

    add: Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Box,
+  Container,
+  Input,
+  Stack,
+  Avatar,
+  Heading,
+  Checkbox,
+  Button,
+  Link
+} from "@chakra-ui/react";
+import { LockIcon } from "@chakra-ui/icons";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box d="flex" alignItems="center" justifyContent="center">
+      <Container maxWidth="400" my="20">
+        <Box d="flex" alignItems="center" flexDirection="column">
+          <Avatar
+            bg="purple"
+            icon={<LockIcon fontSize="1.5rem" color="white" />}
+          />
+          <Heading as="h2" size="lg" fontSize="23" fontWeight="500" my="4">
+            Sign in
+          </Heading>
+        </Box>
+        <Stack spacing={5}>
+          <Input variant="outline" placeholder="Email Address" />
+          <Input variant="outline" placeholder="Password" />
+          <Checkbox defaultIsChecked>Remember me</Checkbox>
+          <Button colorScheme="blue">SIGN IN</Button>
+          <Box d="flex" justifyContent="space-between">
+            <Link color="blue" fontSize="15">Forgot Password?</Link>
+            <Link color="blue" fontSize="15">Didn't have an account? Sign Up</Link>
+          </Box>
+        </Stack>
+      </Container>
+    </Box>
   );
-}
+};
 
 export default App;
